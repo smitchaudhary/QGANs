de10fd2ed47d5905e6e2a6c77e2cf7829ff5f161
Smit Chaudhary <smitchaudhary10@gmail.com>
2021-05-22 17:14:58 +0200

    Added Gate class and full matrix representation

--- gates.py
+++ gates.py
@@ -26,7 +26,7 @@ CNOT = np.matrix([[1, 0, 0, 0],
 def I_n(n):
     return np.eye(2**n)
 
-def single_qubit_rotation(n_qubits, target_qubit, angle, axis):
+def single_qubit_rotation(n_qubits, target_qubit, axis, angle = np.pi):
     if axis == 'X':
         rot = linalg.expm(-0.5j*angle*X)
     elif axis == 'Y':
@@ -45,3 +45,33 @@ def full_CNOT(n_qubits, control, target):
     temp1 = np.kron( I_n(control), np.kron( zero, I_n(n_qubits - control - 1) ) )
     temp2 = np.kron(I_n(control), np.kron(one, np.kron(I_n(target - control - 1), np.kron(X, I_n(n_qubits - target - 1)))))
     return temp1 + temp2
+
+class Gate:
+    def __init__(self, id, control = None, target = None, angle = np.pi):
+        self.id = id
+        self.control = control
+        self.target = target
+        self.angle = angle
+
+    def full_matrix(self, n_qubits):
+        if self.id == 'X' or self.id == 'Y':
+            return 1j*single_qubit_rotation(n_qubits, self.target, self.id)
+        if self.id =='CNOT':
+            return full_CNOT(n_qubits, self.control, self.target)
+
+class Circuit:
+    def __init__(self, n_qubits):
+        self.n_qubits =  n_qubits
+        self.gates = []
+
+    def circ_matrix(self):
+        ans = I_n(n_qubits)
+        for gate in self.gates:
+            ans = np.matul( gate.full_matrix(self.n_qubits) )
+
+        return np.asmatrix(ans)
+
+    def append_gate(self, gate):
+        self.gates.append(gate)
+
+print(single_qubit_rotation(1, 0, 'X'))
